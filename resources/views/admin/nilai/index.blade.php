4901a13b5a69aadf1fb79bc9a13540399ed73561
kiyora <and.syahruddin@gmail.com>
2022-01-13 09:22:31 +0700

    z

--- resources/views/admin/nilai/index.blade.php
+++ resources/views/admin/nilai/index.blade.php
@@ -121,7 +121,7 @@
             <select name="id_matpel" id="" class="form-control">
               <option value="">~~ Option Matpel ~~</option>
               @foreach ($matpel as $m)
-                  <option value="{{$m->id_matpel}}">{{ isset($m->matpel) && !is_null($m->matpel) ? $m->matpel->nama ? : "Tidak ada nama matpel"  }} ( {{ $m->guru->nama }} )</option>
+                  <option value="{{$m->id_matpel}}">{{ isset($m->matpel) && !is_null($m->matpel) ? $m->matpel->nama : "Tidak ada nama matpel"  }} ( {{ $m->guru->nama }} )</option>
               @endforeach
           </select>
           </div>
